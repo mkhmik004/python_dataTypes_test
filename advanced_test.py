9ec93a4b865eae357a99e62bb8483c5e9eac1dfc
Mkhabele Mikejunior Mark Mashaole <137772011+mkhmik004@users.noreply.github.com>
2024-11-11 16:16:34 +0200

    all test pass for advanced_test.py

--- advanced_test.py
+++ advanced_test.py
@@ -6,7 +6,15 @@ def create_squares_of_evens():
     Return:
     - The list of squares of even numbers.
     """
-    pass
+    even_squres=[]
+    for num in range(1,11):
+        if num%2==0:
+            even_squres.append(num**2)
+        else:
+            continue
+    return even_squres
+print(create_squares_of_evens())
+
 
 
 def convert_to_dict(students):
@@ -17,7 +25,10 @@ def convert_to_dict(students):
     Return:
     - The dictionary created from the list of students.
     """
-    pass
+    dict={}
+    for student in students:
+        dict[student[0]]=student[1]
+    return dict
 
 
 def access_value_x(nested):
@@ -28,7 +39,7 @@ def access_value_x(nested):
     Return:
     - The value of 'x' (which is 10).
     """
-    pass
+    return nested['c']['x']
 
 
 def append_to_list_in_dict(nested):
@@ -39,7 +50,9 @@ def append_to_list_in_dict(nested):
     Return:
     - The updated dictionary.
     """
-    pass
+    nested['a'].append(6)
+    return nested
+print(append_to_list_in_dict({'a': [1, 2, 3], 'b': (4, 5), 'c': {'x': 10, 'y': 20}}))
 
 
 def convert_tuple_to_list_and_append(nested):
@@ -50,4 +63,9 @@ def convert_tuple_to_list_and_append(nested):
     Return:
     - The updated dictionary.
     """
-    pass
+    nested['b']=(list(nested['b']))
+    nested['b'].append(6)
+    
+  
+    return nested
+print(convert_tuple_to_list_and_append({'a': [1, 2, 3], 'b': (4, 5), 'c': {'x': 10, 'y': 20}}))
